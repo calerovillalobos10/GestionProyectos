9960e883e81a1e600f1729bc97c242b0a926df2d
leiton-cr <80017848+leiton-cr@users.noreply.github.com>
2022-01-13 17:41:53 -0600

    Agrego diagrama e-r y documento de base de datos.

---
+++ Documentacion/SQL.sql
@@ -0,0 +1,157 @@
+-- DROP DATABASE ProyectManagement
+
+CREATE DATABASE ProyectManagement;
+
+USE ProyectManagement;
+
+CREATE TABLE Sex(
+  idSex TINYINT IDENTITY(1, 1) PRIMARY KEY,
+  [description] VARCHAR(15) NOT NULL,
+);
+
+CREATE TABLE Trimester(
+  idTrimester TINYINT IDENTITY(1, 1) PRIMARY KEY,
+  [description] VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE Departament(
+  idDepartment TINYINT IDENTITY(1, 1) PRIMARY KEY,
+  [description] VARCHAR(30) NOT NULL,
+  [state] BIT DEFAULT 1 NOT NULL
+);
+
+CREATE TABLE FunctionaryType(
+  idFunctionaryType TINYINT IDENTITY(1, 1) PRIMARY KEY,
+  [description] VARCHAR(15) NOT NULL,
+  [state] BIT DEFAULT 1 NOT NULL
+);
+
+CREATE TABLE Functionary(
+  idFunctionary SMALLINT IDENTITY(1, 1) PRIMARY KEY,
+  idSex TINYINT NOT NULL,
+  idDepartment TINYINT NOT NULL,
+  idFunctionaryType TINYINT NOT NULL,
+
+  [name] VARCHAR(15) NOT NULL,
+  surName VARCHAR(15) NOT NULL,
+  lastName VARCHAR(15) NOT NULL,
+  birthday DATE NOT NULL,
+  email VARCHAR(50) UNIQUE NOT NULL,
+  pass VARBINARY(MAX) NOT NULL,
+  urlPhoto VARCHAR(180) NOT NULL,
+  [state] BIT DEFAULT 1 NOT NULL,
+  secretUrl VARCHAR(60) NOT NULL
+
+  CONSTRAINT FuncionarySex -- Funcionary_Sex
+  FOREIGN KEY (idSex) 
+  REFERENCES Sex(idSex),
+
+  CONSTRAINT FuncionaryDepartament  -- Funcionary_Departament
+  FOREIGN KEY (idDepartment) 
+  REFERENCES Departament(idDepartment),
+
+  CONSTRAINT FuncionaryFuncionaryType 
+  FOREIGN KEY (idFunctionaryType) 
+  REFERENCES FunctionaryType(idFunctionaryType)
+);
+
+CREATE TABLE Solicitation(
+  idSolicitation SMALLINT IDENTITY(1, 1) PRIMARY KEY,
+  idFunctionaryA SMALLINT NOT NULL,
+  idFunctionaryR SMALLINT NOT NULL,
+  idFunctionaryF SMALLINT NOT NULL,
+  solicitationDate SMALLDATETIME NOT NULL,
+  startDate DATE NOT NULL,
+  endingDate DATE NOT NULL,
+  document VARBINARY(MAX) NOT NULL,
+  [state] BIT default 1 NOT NULL,
+  finished BIT default 0 NOT NULL,
+  
+  CONSTRAINT FuncionaryASolicitation
+  FOREIGN KEY (idFunctionaryA) 
+  REFERENCES Functionary(idFunctionary),
+  
+  CONSTRAINT FuncionaryRSolicitation 
+  FOREIGN KEY (idFunctionaryR) 
+  REFERENCES Functionary(idFunctionary),
+  
+  CONSTRAINT FuncionaryFSolicitation 
+  FOREIGN KEY (idFunctionaryF) 
+  REFERENCES Functionary(idFunctionary)
+);
+
+CREATE TABLE Advance(
+  idAdvance INTEGER IDENTITY(1, 1) PRIMARY KEY,
+  idTrimester TINYINT NOT NULL,
+  idFunctionaryA SMALLINT NOT NULL,
+  idSolicitation SMALLINT NOT NULL,
+  advanceDate SMALLDATETIME NOT NULL,
+  document VARBINARY(MAX) NOT NULL,
+  [state] BIT default 1 NOT NULL,
+
+  CONSTRAINT AdvanceTrimester 
+  FOREIGN KEY (idTrimester) 
+  REFERENCES Trimester(idTrimester),
+  
+  CONSTRAINT AdvanceSolicitation 
+  FOREIGN KEY (idSolicitation) 
+  REFERENCES Solicitation(idSolicitation),
+  
+  CONSTRAINT FunctionaryAAdvance
+  FOREIGN KEY (idFunctionaryA) 
+  REFERENCES Functionary(idFunctionary)
+);
+
+CREATE TABLE [Transaction](
+  idTransaction TINYINT IDENTITY(1, 1) PRIMARY KEY,
+  [description] VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Binnacle(
+  idBinnacle INTEGER IDENTITY(1, 1) PRIMARY KEY,
+  idTransaction TINYINT NOT NULL,
+  idFunctionaryA SMALLINT NOT NULL,
+  idAdvance INTEGER NOT NULL,
+  idSolicitation SMALLINT NOT NULL,
+  binnacleDate SMALLDATETIME NOT NULL
+
+  CONSTRAINT BinnacleTransaction 
+  FOREIGN KEY (idTransaction) 
+  REFERENCES [Transaction](idTransaction),
+  
+  CONSTRAINT BinnacleFunctionaryA
+  FOREIGN KEY (idFunctionaryA) 
+  REFERENCES Functionary(idFunctionary),
+  
+  CONSTRAINT BinnacleAdvance
+  FOREIGN KEY (idAdvance) 
+  REFERENCES Advance(idAdvance),
+
+  CONSTRAINT BinnacleSolicitation
+  FOREIGN KEY (idSolicitation) 
+  REFERENCES Solicitation(idSolicitation)
+);
+
+
+
+CREATE TABLE SignIn(
+  idFunctionary SMALLINT PRIMARY KEY,
+  [ip] VARCHAR(16) NOT NULL
+
+   CONSTRAINT SignInFunctionary
+  FOREIGN KEY (idFunctionary) 
+  REFERENCES Functionary(idFunctionary)
+);
+
+drop table notification
+
+CREATE TABLE [Notification](
+  idNotification SMALLINT IDENTITY(1, 1) PRIMARY KEY,
+  idFunctionary SMALLINT NOT NULL,
+  [description] VARCHAR(100) NOT NULL,
+  [ip] VARCHAR(16) NOT NULL
+
+  CONSTRAINT NotificationSignIn
+  FOREIGN KEY (idFunctionary) 
+  REFERENCES SignIn(idFunctionary)
+);
